2aac7af64a9500c8c4a71978e4e13f8f05615daa
Mutharasan_Manoharan <98216181+Muthu2312@users.noreply.github.com>
2024-05-06 19:17:07 +0530

    Delete Django directory

--- Django/Registration/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from Registration import views
-
-urlpatterns = [
-    path("",views.Landing.as_view(),name="Landing"),
-    path("Register/",views.Register.as_view(),name="Register"),
-    path("Login/",views.Login.as_view(),name="Login"),
-]
